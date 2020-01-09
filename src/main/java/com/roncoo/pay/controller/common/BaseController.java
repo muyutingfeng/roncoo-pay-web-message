b41f34fabae083f128283a0a9c6475291b797581
muyutingfeng <muyutingfeng@yahoo.com>
2020-01-09 20:55:30 +0800

    init

--- src/main/java/com/roncoo/pay/controller/common/BaseController.java
+++ src/main/java/com/roncoo/pay/controller/common/BaseController.java
@@ -17,6 +17,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 
+
 /**
  * controller基类
  * @author zh
